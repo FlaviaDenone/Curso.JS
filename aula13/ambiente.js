1498693f5becffb86b64fe93d5afcd237eba9a3c
Flavia Denone <flavia_denone@hotmail.com>
2022-10-03 16:13:05 -0300

    Estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*Estrutura de repetção com teste lógico no final*/
+
+var c = 1
+do {
+    console.log(`Passo de ${c}`)
+    c++
+ }while (c <= 6)
+ 
+
+
+
+
+/* Estrutura de repetição com teste logico no inicio
+var c = 1
+while (c <= 6){
+    console.log ('Tudo bem?')
+    c++
+}
+*/
