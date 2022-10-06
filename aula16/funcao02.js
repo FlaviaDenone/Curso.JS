423089036c5050003cd29bc092df6d3e41527e7f
Flavia Denone <flavia_denone@hotmail.com>
2022-10-06 13:47:41 -0300

    Funtcion Vetores

---
+++ aula16/funcao02.js
@@ -0,0 +1,20 @@
+function soma(n1 = 0, n2 = 0){
+    return n1 + n2
+}
+console.log(soma ())
+
+
+
+/*function soma (n1, n2){
+    return n1 + n2
+}
+
+console.log (soma(2)) (neste caso se não for inseridos os numerós para ambos parametros(n1 e n2) o resultado será NaN)
+
+
+
+function soma(n1, n2){   (neste caso n1 recebe 2 e n2 recebe 5 dando o valor 7 no final)
+    return n1 + n2
+}
+
+console.log(soma(2, 5))*/
